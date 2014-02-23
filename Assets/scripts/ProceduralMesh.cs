4d0c4baaed5fd93a34ef91455f892a3b90cfa23a
TORT <tort.spb@gmail.com>
2014-02-24 00:37:14 +0400

    spherical mesh generation + simple height map

--- Assets/scripts/ProceduralMesh.cs
+++ Assets/scripts/ProceduralMesh.cs
@@ -14,29 +14,47 @@ public class ProceduralMesh : MonoBehaviour
 		Mesh mesh = new Mesh ();
 		mf.mesh = mesh;
 
-		// Generate vertices
-		Vector3[] vertices = new Vector3[slices * slices];
-		Vector3[] normals = new Vector3[slices * slices];
-		Vector2[] uv = new Vector2[slices * slices];
-		for (int j = 0; j < slices; ++j)
+		float[] heightMap = new float[slices * slices];
+		for(int j = 0; j < slices; ++j)
 		{
-			float v = (float)j / (float)slices;
-			float theta = Mathf.PI * v;
-			for (int i=0; i<slices; ++i)
+			for(int i = 0; i < slices; ++i)
 			{
 				int index = j * slices + i;
-				float u = (float)i / (float)slices;
+				heightMap[index] = Random.Range(0.91f,1.01f);
+			}
+		}
+
+		// Generate vertices
+		int vertCountI = slices + 1;
+		int vertCountJ = slices;
+		int vertCount = vertCountI * vertCountJ;
+		Vector3[] vertices = new Vector3[vertCount];
+		Vector3[] normals = new Vector3[vertCount];
+		Vector2[] uv = new Vector2[vertCount];
+		for (int j = 0; j < vertCountJ; ++j)
+		{
+			float v = (float)j / (float)(vertCountJ - 1); // [0..1]
+			//float z = 1.0f - v * 2.0f;
+			//float theta = Mathf.Acos (z);
+			float theta = Mathf.PI * v;
+			for (int i=0; i < vertCountI; ++i)
+			{
+				int mapIndex = j * slices + (i == slices ? 0 : i);
+				int index = j * vertCountI + i;
+				float u = (float)i / (float)(vertCountI - 1); // [0..1]
 				float phi = 2.0f * Mathf.PI * u;
 				Vector3 point = new Vector3 (
-						Mathf.Sin (theta) * Mathf.Cos (phi),
 						Mathf.Sin (theta) * Mathf.Sin (phi),
-						Mathf.Cos (theta)
+						Mathf.Cos (theta),
+						Mathf.Sin (theta) * Mathf.Cos (phi)
 					);
-				vertices[index] = point * radius;
+				vertices[index] = point * radius * heightMap[mapIndex];
 				normals[index] = point;
-				uv[index] = new Vector2(u,v);
+				uv[index] = new Vector2(1.0f - u, 1.0f - v);
 
-				Debug.Log(string.Format("{0}: {1},{2},{3}", index, point.x, point.y, point.z));
+				// Polar texture coords fix
+				if(j == 0) uv[index].x -= 0.5f/(float)(vertCountI - 1);
+				if(j == vertCountJ - 1) uv[index].x += 0.5f/(float)(vertCountI - 1);
 			}
 		}
 		mesh.vertices = vertices;
@@ -44,25 +62,52 @@ public class ProceduralMesh : MonoBehaviour
 		mesh.uv = uv;
 
 		// Generate triangles
-		int[] tri = new int[ (slices - 1) * (slices) * 6 ];
-		for (int j = 0; j < slices - 1; ++j)
+		int triCountI = vertCountI - 1;
+		int triCountJ = vertCountJ - 1;
+		int triCount = triCountI * triCountJ;
+		int[] tri = new int[ triCount * 6 ];
+		for (int j = 0; j < triCountJ - 2; ++j)
 		{ 
-			for (int i = 0; i < slices; ++i)
+			for (int i = 0; i < triCountI; ++i)
 			{
-				int vertIndex = j * slices + i;
-				int vertIndexNext = j * slices + ((i == slices - 1) ? 0 : i + 1);
+				int vertIndex = (j + 1) * vertCountI + i;
+				int vertIndexNextI = (j + 1) * vertCountI + (i + 1);
+				int vertIndexNextJ = (j + 2) * vertCountI + i;
+				int vertIndexNextIJ = (j + 2) * vertCountI + (i + 1);
 
-				int triIndex = (j * (slices - 1) + i) * 6;
+				int triIndex = (j * triCountI + i) * 6;
 
 				tri[triIndex++] = vertIndex;
-				tri[triIndex++] = vertIndex + slices;
-				tri[triIndex++] = vertIndexNext;
+				tri[triIndex++] = vertIndexNextJ;
+				tri[triIndex++] = vertIndexNextI;
 
-
-				tri[triIndex++] = vertIndex + slices;
-				tri[triIndex++] = vertIndexNext + slices;
-				tri[triIndex++] = vertIndexNext;
+				tri[triIndex++] = vertIndexNextJ;
+				tri[triIndex++] = vertIndexNextIJ;
+				tri[triIndex++] = vertIndexNextI;
 			}
+		}
+		// Polar tiangle fans
+		for (int i = 0; i < triCountI; ++i)
+		{
+			int vertIndex = (vertCountJ - 2) * vertCountI + i;
+			int vertIndexNextI = (vertCountJ - 2) * vertCountI + (i + 1);
+			int vertIndexNextJ = (vertCountJ - 1) * vertCountI + i;
+			int vertIndexNextIJ = (vertCountJ - 1) * vertCountI + (i + 1);
+			
+			int triIndex = ((triCountJ - 2) * triCountI + i) * 6;
+			
+			tri[triIndex++] = vertIndex;
+			tri[triIndex++] = vertIndexNextJ;
+			tri[triIndex++] = vertIndexNextI;
+
+			vertIndex = i;
+			vertIndexNextI = (i + 1);
+			vertIndexNextJ = 1 * vertCountI + i;
+			vertIndexNextIJ = 1 * vertCountI + (i + 1);
+			
+			tri[triIndex++] = vertIndexNextJ;
+			tri[triIndex++] = vertIndexNextIJ;
+			tri[triIndex++] = vertIndexNextI;
 		}
 		mesh.triangles = tri;
 	}
