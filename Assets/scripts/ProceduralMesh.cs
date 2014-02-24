0b0f008aabc92c9aa669f6acb6454d378fab471d
tort32 <tort.biz@gmail.com>
2014-02-24 18:56:31 +0400

    mesh generation by heightmap + skybox

--- Assets/scripts/ProceduralMesh.cs
+++ Assets/scripts/ProceduralMesh.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class ProceduralMesh : MonoBehaviour
 {
@@ -13,14 +13,135 @@ public class ProceduralMesh : MonoBehaviour
 		MeshFilter mf = GetComponent<MeshFilter> ();
 		Mesh mesh = new Mesh ();
 		mf.mesh = mesh;
+		
+		// Build displacement map
+		Texture2D heightMap = mf.renderer.material.GetTexture (0) as Texture2D;
+		/*Texture2D heightMap = new Texture2D (256, 256);
 
-		float[] heightMap = new float[slices * slices];
+		// set texture
+		Color[] cols = new Color[heightMap.width * heightMap.height];
+		Color[] colsOut = new Color[heightMap.width * heightMap.height];
+		for(int x = 0; x < heightMap.width; ++x)
+			for(int y = 0; y < heightMap.height; ++y)
+		{
+			float value = Random.Range(0.0f,1.0f);
+			cols[y * heightMap.width + x] = new Color(value,value,value);
+		}
+		
+		Vector3 spherePoints = new Vector3[heightMap.width * heightMap.height];
+		for (int y = 0; y < heightMap.height; ++y)
+		{
+			float v = (float)y / (float)heightMap.height; // [0..1)
+			float theta = Mathf.PI * v;
+			for (int x = 0; x < heightMap.width; ++x)
+			{
+				int mapIndex = (heightMap.height - y) * heightMap.width + (heightMap.width - x);
+				float u = (float)x / (float)heightMap.width; // [0..1)
+				float phi = 2.0f * Mathf.PI * u;
+				Vector3 point = new Vector3 (
+					Mathf.Sin (theta) * Mathf.Sin (phi),
+					Mathf.Cos (theta),
+					Mathf.Sin (theta) * Mathf.Cos (phi)
+					);
+				spherePoints[mapIndex] = point;
+			}
+		}
+
+		// Build oct grid to optimize search
+
+		int octSize = 8;
+		List<int>[] octGrid = new List<int>[octSize*octSize*octSize];
+		for(int n = 0; n < heightMap.width * heightMap.height; ++n)
+		{
+			int x = Mathf.Min( (spherePoints[n].x - 1.0f)/2.0f * octSize, octSize-1);
+			int y = Mathf.Min( (spherePoints[n].y - 1.0f)/2.0f * octSize, octSize-1);
+			int z = Mathf.Min( (spherePoints[n].z - 1.0f)/2.0f * octSize, octSize-1);
+			List<int> indexList = octGrid[z*octSize*octSize + y*octSize + x];
+			if(indexList == null)
+			{
+				indexList = new List<int>(n);
+			}
+			else
+			{
+				indexList.Add(n);
+			}
+		}
+
+		// Compute texture smooth with oct grid search
+		float smoothSize = 0.1;
+		float s2 = smoothSize * smoothSize;
+		float nextCels = Mathf.CeilToInt(smoothSize * 0.5f * (float)octGrid);
+		for(int n = 0; n < heightMap.width * heightMap.height; ++n)
+		{ 
+			Vector3 point = spherePoints[n];
+			int x = Mathf.Min( (point.x - 1.0f)/2.0f * octSize, octSize-1);
+			int y = Mathf.Min( (point.y - 1.0f)/2.0f * octSize, octSize-1);
+			int z = Mathf.Min( (point.z - 1.0f)/2.0f * octSize, octSize-1);
+
+			int minX = Mathf.Max(0, x - nextCels); int maxX = Mathf.Min(x + nextCels, octSize);
+			int minY = Mathf.Max(0, y - nextCels); int maxY = Mathf.Min(y + nextCels, octSize);
+			int minZ = Mathf.Max(0, z - nextCels); int maxZ = Mathf.Min(z + nextCels, octSize);
+
+			float accum = 0;
+			float mass = 0;
+			for(int octX = minX; octX <= maxX; ++octX)
+			{
+				for(int octY = minY; octY <= maxY; ++octY)
+				{
+					for(int octZ = minZ; octZ <= maxZ; ++octZ)
+					{
+						List<int> indexList = octGrid[z*octSize*octSize + y*octSize + x];
+						if(indexList == null)
+							break;
+
+						foreach(int index in indexList)
+						{
+							Vector3 pointNear = spherePoints[index];
+							float l2 = (point - pointNear).sqrMagnitude;
+							if(l2 < smoothSize*smoothSize)
+							{
+								float weight = 2.0f/(1.0f + l2 / s2) - 1.0f;
+								accum += Color[n].r * weight;
+								mass += weight;
+							}
+						}
+					}
+				}
+			}
+			float value = accum/mass;
+			colsOut[n] = new Color(value,value,value);
+		}
+
+
+		for(int x = 0; x < heightMap.width; ++x)
+			for(int y = 0; y < heightMap.height; ++y)
+		{
+			float value = Random.Range(0.0f,1.0f);
+			cols[y * heightMap.width + x] = new Color(value,value,value);
+		}
+
+		heightMap.SetPixels (cols);
+		heightMap.Apply ();
+		mf.renderer.material.mainTexture = heightMap;
+		*/
+
+		float[] dispMap = new float[slices * slices];
+		int texPerVertX = heightMap.width / slices;
+		int texPerVertY = heightMap.height / slices;
 		for(int j = 0; j < slices; ++j)
 		{
 			for(int i = 0; i < slices; ++i)
 			{
 				int index = j * slices + i;
-				heightMap[index] = Random.Range(0.91f,1.01f);
+				float height = 0;
+				for(int x = 0; x < texPerVertX; ++x)
+					for(int y = 0; y < texPerVertY; ++y)
+					{
+					Color pixel = heightMap.GetPixel(	heightMap.width - (i*texPerVertX + x - texPerVertX/2),
+					                                 	heightMap.height - (j*texPerVertY + y - texPerVertY/2) );
+						height += pixel.r;
+					}
+				dispMap[index] = height/(texPerVertX*texPerVertY);
 			}
 		}
 
@@ -48,7 +169,7 @@ public class ProceduralMesh : MonoBehaviour
 						Mathf.Cos (theta),
 						Mathf.Sin (theta) * Mathf.Cos (phi)
 					);
-				vertices[index] = point * radius * heightMap[mapIndex];
+				vertices[index] = point * (1.0f + dispMap[mapIndex]) * radius;
 				normals[index] = point;
 				uv[index] = new Vector2(1.0f - u, 1.0f - v);
 
